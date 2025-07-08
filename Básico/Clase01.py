c4b9f4cff0fbb92b3412762011549f6bb177c782
BloodyMCode <mary.reyes.1521@gmail.com>
2025-07-08 12:44:54 -0600

    Update Clase01.py

--- Básico/Clase01.py
+++ Básico/Clase01.py
@@ -1,1 +1,19 @@
+""" Nota: En este apartado se muestran 3 sentencias básicas de programación para principiantes """
+""" Impresión de mensaje """
+mensaje="Clase 01 Python, como imprimir en terminal"
+print(mensaje)
 
+""" Sitaxis básica, operadores"""
+resultado1=5+6; """ suma """
+resultado2=5*6; """ multiplicación """
+resultado3=5/2; """ división """
+resultado4=5%2; """ modulo """
+resultado5=5**3; """ potencia """
+final=resultado1,resultado2,resultado3,resultado4,resultado5
+print (final)
+
+""" Condicional sencilla """
+num1=3
+num2=6
+if num1>num2:print("si es mayor")
+else: print("no es mayor")
